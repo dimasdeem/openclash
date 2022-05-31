b20c4238fe368aebe97ea7b9914168e9d49b7b8a
dimasdeem <92859246+dimasdeem@users.noreply.github.com>
2022-05-31 11:45:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 pkg update
 pkg upgrade -y
 pkg install wget -y
-wget https://raw.githubusercontent.com/dimasdeem/openclash
-cd openclash
+wget https://raw.githubusercontent.com/dimasdeem/openclash/main/openclash
+chmod +x openclash
+mkdir .config/clash/lb
+mkdir .config/clash/a
+mkdir .config/clash/vm
+mv openclash /data/data/com.termux/files/usr/bin/
+clear
+wget https://raw.githubusercontent.com/R1punk/clash/main/config.yaml
+mv config.yaml /.config/clash/lb
+clear
